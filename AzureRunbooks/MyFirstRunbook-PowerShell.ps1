51a9f496e51c525e9c0213bfdfbcb34c9e1043e4
kernsd <kernsdiii@yahoo.com>
2017-05-18 11:26:19 -0400

    Update MyFirstRunbook-PowerShell.ps1

--- AzureRunbooks/MyFirstRunbook-PowerShell.ps1
+++ AzureRunbooks/MyFirstRunbook-PowerShell.ps1
@@ -1,8 +1,8 @@
 Param(
- [string]$VMName,
+ [string]$VMName3,
  [string]$ResourceGroupName
 )
 $Conn = Get-AutomationConnection -Name AzureRunAsConnection
 Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID `
 -ApplicationID $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
-Start-AzureRmVM -Name $VMName -ResourceGroupName $ResourceGroupName
+Start-AzureRmVM -Name $VMName3 -ResourceGroupName $ResourceGroupName
